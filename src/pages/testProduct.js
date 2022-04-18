3061ce55099dc184c2b64780138da171fe94fd82
Almoll <alvaro.molla.delrio@gmail.com>
2022-04-18 17:08:58 +0200

    p'ara carrit

--- src/pages/testProduct.js
+++ src/pages/testProduct.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/styles.css';
 
+
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
 
@@ -13,37 +14,31 @@ import { Link } from "react-router-dom";
 import CarouselPaint from '../components/CarouselArt';
 import Footer from '../components/footer';
 import blacksad from '../images/home/1.jpg';
-
-const Home = () => {
+const testProduct = () => {
   return (
-    <> 
+    <>    
+<br/>
 
-    {/*
-    <h1 class="center">
-      Destacados
-    </h1>
- <h1>Zoom on Hover</h1>
-<p>Hover over the div element.</p>
-  
-<div class="zoom col-md-3"><img src={blacksad}/></div>
-*/}
+<p><a href="/home">Home</a> / <a href="/testProduct">carrito</a></p><p/>
 
-<br/><br/>
-<p><a href="/home">Home</a> / <a href="/paint">Pinturas</a> / <a href="/testProduct">Cuadro 1</a> </p><p/>
-<div class="row">
-  <div class="col-md-3 zoom">
-      <div class="card-body">
-      <div class="product-2 align-items-center p-2 text-center">
-      <img src={blacksad} alt="" class="rounded img-fluid" width="400"/>
-    </div>
- </div> 
+
+<section class="container content-section">
+<h2 class="section-header">CART</h2>
+<div class="cart-row">
+    <span class="cart-item cart-header cart-column">ITEM</span>
+    <span class="cart-price cart-header cart-column">PRICE</span>
+    <span class="cart-quantity cart-header cart-column">QUANTITY</span>
 </div>
+<div class="cart-items">
 </div>
-
-
+<div class="cart-total">
+    <strong class="cart-total-title">Total</strong>
+    <span class="cart-total-price">$0</span>
+</div>
+<button class="btn btn-primary btn-purchase" type="button">PURCHASE</button>
+</section>
+<Footer/>
     </>
-    
   );
 };
-
-export default Home;
+export default testProduct;
